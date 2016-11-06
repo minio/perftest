0f233631fe51240c2e5a1f1f0f6d539739b86d42
Karthic Rao <karthic@minio.io>
2016-11-07 03:39:06 +0530

    Adding exec-concurrent test. (#2)

---
+++ exec-concurrent/README.md
@@ -0,0 +1,57 @@
+# exec-concurent tests. 
+
+- Runs Minio-go functional tests concurrently on the directed Minio instances generating load.
+
+# Instructions to run.
+
+- Set the access Key.
+  ```sh
+  export ACCESS_KEY=xxxxxx
+  ```
+- Set the Secret Key.
+  ```sh
+  export SECRET_KEY=xxxxxxx
+  ```
+- Set the Minio server endpoint.
+  ```sh
+  export ENDPOINT=xxx.xx.xxx.xxx:<PORT>
+  ```
+- Set concurrent level of the load.
+  ```sh
+  export CONCURRENCY=100
+  ```
+- Build the api_functional_v4_test.go
+  ```sh
+  $ go test -c api_functional_v4_test.go 
+  ```
+- Build exec-concurrent.go
+  ```sh
+  $ go build exec-concurrent.go
+  ```
+- Run the test.
+  ```sh
+  $ ./exec-concurrent <TestName>
+  ```
+- Here are the list of supported   TestNAMES. 
+
+  ```
+  TestMakeBucketError
+  TestMakeBucketRegions
+  TestPutObjectReadAt
+  TestListPartiallyUploaded
+  TestGetOjectSeekEnd
+  TestGetObjectClosedTwice
+  TestRemovePartiallyUploaded
+  TestResumablePutObject
+  TestResumableFPutObject
+  TestFPutObjectMultipart
+  TestFPutObject
+  TestGetObjectReadSeekFunctional
+  TestGetObjectReadAtFunctional
+  TestPresignedPostPolicy
+  TestCopyObject
+  TestFunctional
+  ```  
+  
+
+     
---
+++ exec-concurrent/exec-concurrent.go
@@ -0,0 +1,63 @@
+/*
+ * Minio Cloud Storage, (C) 2015, 2016 Minio, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"sync"
+)
+
+func init() {
+	os.Setenv("ACCESS_KEY", os.Getenv("ACCESS_KEY"))
+	os.Setenv("SECRET_KEY", os.Getenv("SECRET_KEY"))
+	os.Setenv("ENDPOINT", os.Getenv("ENDPOINT"))
+	os.Setenv("CONCURRENCY", os.Getenv("CONCURRENCY"))
+}
+
+func main() {
+	concurrency, err := strconv.Atoi(os.Getenv("CONCURRENCY"))
+	if err != nil {
+		fmt.Println("Please set a valid integer for concurrency level. ex: `export CONCURRENCY=100`: ", err)
+		return
+	}
+	var wg sync.WaitGroup
+	f, _ := os.Create("output.log")
+	defer f.Close()
+	testCmd := "./minio.test -test.timeout 3600s"
+	if len(os.Args[1]) != 0 {
+		testCmd = fmt.Sprintf("./minio.test -test.timeout 3600s -test.run %s", os.Args[1])
+	}
+	for i := 0; i < concurrency; i++ {
+		wg.Add(1)
+		go func(routineId int) {
+			defer wg.Done()
+
+			out, err := exec.Command("sh", "-c", testCmd).Output()
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			fmt.Fprintf(f, "\nGoroutine: %d\n", routineId+1)
+			fmt.Fprintf(f, string(out))
+
+		}(i)
+	}
+	wg.Wait()
+
+}
